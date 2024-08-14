e4b6d4287909850475a91725f07954d1f39b4f59
sscc20111 <mw92559404@gmail.com>
2024-08-15 02:19:03 +0900

    Publishing - feat: Main - html, css 작성 (section01) - build: create app, bootstrap, gsap 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Main from './page/main';
+import GuestBook from './page/guestbook';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='ContentsBody'>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="guestbook" element={<GuestBook />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/page/guestbook/index.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const GuestBook = () => {
+    return(
+        <>
+            <div>Guestbook Page</div>
+            <Link to='/'>Main Page Link</Link>
+        </>
+    )
+}
+
+export default GuestBook;
