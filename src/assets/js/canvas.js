09dfb8838184ed1c7c038a4085911b5a53ad2848
sscc20111 <mw92559404@gmail.com>
2024-08-23 04:43:47 +0900

    Frontend - fix: canvas.js 수정(point 에서 wave로 기능 추가 )

--- src/assets/js/canvas.js
+++ src/assets/js/canvas.js
@@ -7,34 +7,41 @@ const canvasJs = (selecter, option) => {
     //setting
     canvas.width = canvas.height = option.size;
     ctx.fillStyle = "white";//임시
+    const total = 5;
 
 
     //변동상수
     let angle = 0;
-    let y = canvas.height/2
-
 
     const draw = () => {
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath()
-        ctx.arc(canvas.width/2, y, 5, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
+        Array.from({ length: total }).forEach((_, index) => {
+            ctx.arc(canvas.width*(index)/(total-1), update(index), 5, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        });
 
-        update();
+
     };
 
-    const update = () => {
+    const update = (index) => {
+        let y = canvas.height/2; //변동 y
+        const FixY = canvas.height/2; //고정 y
+        
         const limit = 100; //움직임 반경
-        const speed = 0.05;
-
-        y = canvas.height/2 + Math.sin(angle) * limit; //y = 시작점 + 변동값
+        const speed = 0.01; //속도 제어
+        
+        //y = 시작점 + 변화값(+index로 각 점의 시작점 차등분배) + 최대범위
+        y = FixY + (Math.sin(angle+(index)) * limit); 
         angle += speed;
-
+        
         //angle 무한 증가 방지
         if (angle >= Math.PI * 2) {
             angle -= Math.PI * 2;
         }
+        return y
     };
 
     setInterval(draw, 15);
