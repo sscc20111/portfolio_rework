d03ba6133f185f778495539241adf9b8552e72d2
sscc20111 <mw92559404@gmail.com>
2024-08-24 03:31:15 +0900

    Frontend - fix: canvas.js (계산식 오류 수정, 불필요 식 제거)

--- src/assets/js/canvas.js
+++ src/assets/js/canvas.js
@@ -9,17 +9,16 @@ const canvasJs = (selecter, option) => {
     const CentserX = canvas.width/2;  //중심 x
     const CentserY = canvas.height/2; //중심 y
 
-    const total = 6; //point 개수
+    const total = 10; //point 개수
     const limit = 30; //움직임 반경
-    const PointSpeed = 0.01; //point속도 제어
-    const rotation = 0.001; //축 회전속도 제어
+    const PointSpeed = 0.002; //point속도 제어
+    const rotationSpeed = 0.0003; //축 회전속도 제어
 
     //변동상수
     let angle = 0;
     let rotat = 0;
 
     //임시
-    const radian = 2*Math.PI;//θ
     const L = 100; //반지름
 
     
@@ -43,7 +42,7 @@ const canvasJs = (selecter, option) => {
         ctx.beginPath()
         Array.from({ length: total }).forEach((_, index) => {
             ctx.moveTo(canvas.width/2, canvas.height/2);
-            ctx.lineTo(CentserX + (L*Math.cos((radian / total *index)+rotat)), CentserY + (L*Math.sin((radian / total *index)+rotat)));
+            ctx.lineTo(CentserX + ((L+limit)*Math.cos((2*Math.PI / total *index)+rotat)), CentserY + ((L+limit)*Math.sin((2*Math.PI / total *index)+rotat)));
         });
         ctx.stroke()
         ctx.closePath();
@@ -53,18 +52,24 @@ const canvasJs = (selecter, option) => {
         //직각 삼각형의 좌표평면상 점(x,y)를 구하는 공식을 사용하여 함수 실행
         //x = 빗변길이 * conθ
         //y = 빗변길이 * sinθ
-        let FixedX = CentserX + (L*Math.cos((radian / total *index)+rotat)); //변동x 기준점 //+rotat 축 회전(점θ 회전에는 영향을 주지않음)
-        let FixedY = CentserY + (L*Math.sin((radian / total *index)+rotat)); //변동y 기준점 //+rotat 축 회전(점θ 회전에는 영향을 주지않음)
-        let x = CentserY + (L*Math.cos(radian / total *index)); //변동x
-        let y = CentserX + (L*Math.sin(radian / total *index)); //변동y
-        let wave = Math.sin(angle+(index)) * limit; //wave = sin값(0~1) * 움직임 반경 //angle+(index) 차등 움직임
-        
-        x = FixedX + Math.cos((radian / total *index)+rotat) * wave; //+rotat 점θ 회전(축 회전에는 영향을 주지않음)
-        y = FixedY + Math.sin((radian / total *index)+rotat) * wave; //+rotat 점θ 회전(축 회전에는 영향을 주지않음)
+
+
+        const radian = 2*Math.PI * index / total; //2π*(index/total)
+        let wave = Math.sin(angle+radian) * limit; //(0~1)*(움직임 반경) //sinθ로 부드럽게
+
+        //(x,y)좌표 축 계산
+        let FixedX = CentserX + (L * Math.cos(radian+rotat)); //+rotat 축 회전(점θ 회전에는 영향을 주지않음)
+        let FixedY = CentserY + (L * Math.sin(radian+rotat)); //+rotat 축 회전(점θ 회전에는 영향을 주지않음)
+
+        //(x,y)좌표 계산
+        let x = FixedX + Math.cos(radian+rotat) * wave; //+rotat 점θ 회전(축 회전에는 영향을 주지않음)
+        let y = FixedY + Math.sin(radian+rotat) * wave; //+rotat 점θ 회전(축 회전에는 영향을 주지않음)
+
+        //움직임 제어
         angle += PointSpeed;
-        rotat += rotation;
-        
-        //angle 무한 증가 방지
+        rotat += rotationSpeed;
+
+        //angle, rotat 무한 증가 방지
         if (angle >= Math.PI * 2) {
             angle -= Math.PI * 2;
         }
